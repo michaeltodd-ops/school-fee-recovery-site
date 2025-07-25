bbb363b627bc379f2a8a28a7a900b5dcb6f16235
Michael Todd <mlt@opsgl.com>
2025-07-25 14:08:38 +1000

    Update ThankYou.js

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function ThankYou() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Thank You template</h1>
+      <p>This is a placeholder for the Thank You template. Real content will go here.</p>
+    </div>
+  );
+}
+
+export default ThankYou;
 
